d1fc4aaa36e17d37ec51ce8c3d0a38447714910f
no9 <anton@venshare.com>
2015-06-16 01:26:51 +0100

    code clean and bigger hello world for mobile app

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 #! /usr/bin/env node
 var shell = require('shelljs')
 var os = require('os')
-var path = require('path')
 var args = process.argv.slice(2).join(' ')
 
 switch (process.argv[2]) {
   case 'gen':
-    shell.cp('-R', __dirname + '/templates/*', process.argv[3]);
+    shell.cp('-R', __dirname + '/templates/*', process.argv[3])
   break
 
   case 'init':
--- test/src/background.js
+++ test/src/background.js
@@ -10,8 +10,8 @@ chrome.app.runtime.onLaunched.addListener(function () { // eslint-disable-line
 var http = require('http')
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World\n')
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end('<h1>Hello World</h1>\n')
 }).listen(1337, '127.0.0.1')
 
 console.log('Server running at http://127.0.0.1:1337/')
